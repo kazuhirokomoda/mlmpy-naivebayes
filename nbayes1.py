f02f6a44fc41dde343fe2928eb1ca2d590ffe4d4
kazuhirokomoda <kazuhirokomoda@gmail.com>
2014-10-05 19:21:42 +0900

    class NaiveBayes1

--- nbayes1.py
+++ nbayes1.py
@@ -8,3 +8,76 @@ http://www.kamishima.net/mlmpyja/
 """
 
 import numpy as np
+
+class NaiveBayes1(object):
+	"""
+	Naive Bayes class (1)
+	"""
+
+	def __init__(self):
+		self.pY_ = None
+		self.pXgY_ = None
+
+
+	def fit(self, X, y):
+		"""
+		Fitting model
+		"""
+
+		n_samples = X.shape[0]
+		n_features = X.shape[1]
+		# TODO improve assumptions
+		n_classes = 2 # C
+		n_fvalues = 2 # K
+
+		# check the size of y
+		if n_samples != len(y):
+			raise ValueError('Mismatched number of samples.')
+
+		## train class distribution
+		# count up n[yi=y]
+		nY = np.zeros(n_classes, dtype=np.int)
+		for i in xrange(n_samples):
+			nY[y[i]] += 1
+
+		# calculate pY_
+		self.pY_ = np.empty(n_classes, dtype=np.float)
+		for yi in xrange(n_classes):
+			self.pY_[yi] = nY[yi] / np.float(n_samples)
+
+		## train feature distribution
+		# count up 
+		nXY = np.zeros((n_features, n_fvalues, n_classes), dtype=np.int)
+		for i in xrange(n_samples):
+			for j in xrange(n_features):
+				nXY[j, X[i,j], y[i]] += 1
+
+		# calculate pXgY_
+		self.pXgY_ = np.empty((n_features, n_fvalues, n_classes), dtype=np.float)
+		for j in xrange(n_features):
+			for xi in xrange(n_fvalues):
+				for yi in xrange(n_classes):
+					self.pXgY_ = nXY[j, xi, yi] / np.float(nY[yi])
+
+
+	def predict(self, X):
+		"""
+		Predict class
+		"""
+
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
+
+        # memory for return values
+        y = np.empty(n_samples, dtype=np.int)
+
+        # for each feature in X
+        for i, xi in enumerate(X):
+        	# calculate joint probability
+        	logpXY = np.log(self.pY_) + np.sum(np.log(self.pXgY_[np.arange(n_features), xi, :]), axis=0)
+
+        	# predict class
+        	y[i] = np.argmax(logpXY)
+
+        return y	
+
