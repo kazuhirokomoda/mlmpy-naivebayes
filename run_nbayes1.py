9ea6f78822ac29b853c9341ac681cb5f8b0be80a
kazuhirokomoda <kazuhirokomoda@gmail.com>
2014-10-05 20:30:19 +0900

    modify run script and add data

--- run_nbayes1.py
+++ run_nbayes1.py
@@ -8,3 +8,24 @@ http://www.kamishima.net/mlmpyja/
 """
 
 import numpy as np
+from nbayes1 import NaiveBayes1
+
+
+# load date
+# http://docs.scipy.org/doc/numpy/user/basics.io.genfromtxt.html
+data = np.genfromtxt('vote_filled.tsv', dtype=np.int)
+
+# split data
+X = data[:, :-1]
+y = data[:, -1]
+
+# learn model
+clr = NaiveBayes1()
+clr.fit(X, y)
+
+# test model
+predict_y = clr.predict(X[:10, :])
+
+# print results
+for i in xrange(10):
+  print i, y[i], predict_y[i]
