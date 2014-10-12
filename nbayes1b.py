8c55491e8c7ba7716ad45bbe25a94084b51c34c3
kazuhirokomoda <kazuhirokomoda@gmail.com>
2014-10-13 00:07:25 +0900

    add NaiveBayes2. for loops are eliminated. amazing.

--- nbayes1b.py
+++ nbayes1b.py
@@ -11,7 +11,7 @@ import numpy as np
 from abc import ABCMeta, abstractmethod
 
 # public symbols
-__all__ = ['BaseBinaryNaiveBayes', 'NaiveBayes1']
+__all__ = ['BaseBinaryNaiveBayes', 'NaiveBayes1', 'NaiveBayes2']
 
 
 class BaseBinaryNaiveBayes(object):
@@ -101,3 +101,118 @@ class NaiveBayes1(BaseBinaryNaiveBayes):
         for yi in xrange(n_classes):
           self.pXgY_[j, xi, yi] = nXY[j, xi, yi] / np.float(nY[yi])
 
+
+class NaiveBayes2(BaseBinaryNaiveBayes):
+  """
+  Naive Bayes class (2)
+  """
+
+  def __init__(self):
+    super(NaiveBayes2, self).__init__()
+
+  def fit(self, X, y):
+    """
+    Fitting model
+    """
+
+    n_samples = X.shape[0]
+    n_features = X.shape[1]
+    # TODO improve assumptions
+    n_classes = 2 # C
+    n_fvalues = 2 # K
+
+    # check the size of y
+    if n_samples != len(y):
+      raise ValueError('Mismatched number of samples.')
+
+    ## train class distribution
+    # count up n[yi=y]
+
+    # (1) クラスの対応する添え字の要素のカウンタを一つずつ増やす
+    """
+    nY = np.zeros(n_classes, dtype=np.int)
+    for i in xrange(n_samples):
+      nY[y[i]] += 1
+    """
+
+    # (2) 各クラスごとに，現在の対象クラスの事例であったらなら対応する要素のカウンタを一つずつ増やす
+    """
+    nY = np.zeros(n_classes, dtype=np.int)
+    for yi in xrange(n_classes):
+      for i in xrange(n_samples):
+        if y[i] == yi:
+          nY[yi] += 1
+      # using universal function np.equal(==) and np.sum,
+      # i for-loop can be eliminated. (nY can be defined as np.empty)
+      #nY[yi] = np.sum(y==yi)
+    """
+
+    # (3) eliminating for loop from (2).
+    # ary_i = np.arange(n_samples)[:, np.newaxis]
+    # ary_y = y[ary_i]
+    ary_y = y[:, np.newaxis]
+    ary_yi = np.arange(n_classes)[np.newaxis, :]
+    cmp_y = (ary_y == ary_yi)
+    nY = np.sum(cmp_y, axis=0)
+
+
+    # calculate pY_
+    # (1)
+    """
+    self.pY_ = np.empty(n_classes, dtype=np.float)
+    for yi in xrange(n_classes):
+      self.pY_[yi] = nY[yi] / np.float(n_samples)
+    """
+    # (2)
+    self.pY_ = np.true_divide(nY, n_samples)
+
+
+    ## train feature distribution
+    # count up n[x_ij=xj, yi=y]
+    # (1)
+    """
+    nXY = np.zeros((n_features, n_fvalues, n_classes), dtype=np.int)
+    for i in xrange(n_samples):
+      for j in xrange(n_features):
+        nXY[j, X[i,j], y[i]] += 1
+    """
+
+    # (2) クラスの分布の場合と同様に，各特徴値ごとに，対象の特徴値の場合にのみカウンタを増やすような実装にします．
+    """
+    nXY = np.zeros((n_features, n_fvalues, n_classes), dtype=np.int)
+    for i in xrange(n_samples):
+      for j in xrange(n_features):
+        for yi in xrange(n_classes):
+          for xi in xrange(n_fvalues):
+            if y[i]==yi and X[i,j]==xi:
+              nXY[j, xi, yi] += 1
+    """
+
+    # (3) eliminating for loop from (2).
+    ary_xi = np.arange(n_fvalues)[np.newaxis, np.newaxis, :, np.newaxis]
+    ary_yi = np.arange(n_classes)[np.newaxis, np.newaxis, np.newaxis, :]
+    #ary_i = np.arange(n_samples)[:, np.newaxis, np.newaxis, np.newaxis]
+    #ary_y = y[ary_i]
+    ary_y = y[:, np.newaxis, np.newaxis, np.newaxis]
+    # X の要素を，ループを割り当てた次元に対応するように配置した配列を直接的に生成する
+    ary_X = X[:, :, np.newaxis, np.newaxis]
+
+    cmp_X = (ary_X == ary_xi)
+    cmp_y = (ary_y == ary_yi)
+    cmp_Xandy = np.logical_and(cmp_X, cmp_y)
+
+    nXY = np.sum(cmp_Xandy, axis=0)
+
+
+    # calculate pXgY_
+    # (1)
+    """
+    self.pXgY_ = np.empty((n_features, n_fvalues, n_classes), dtype=np.float)
+    for j in xrange(n_features):
+      for xi in xrange(n_fvalues):
+        for yi in xrange(n_classes):
+          self.pXgY_[j, xi, yi] = nXY[j, xi, yi] / np.float(nY[yi])
+    """
+
+    # (2)
+    self.pXgY_ = np.true_divide(nXY, nY[np.newaxis, np.newaxis, :])
--- run_nbayes1b.py
+++ run_nbayes1b.py
@@ -8,7 +8,7 @@ http://www.kamishima.net/mlmpyja/
 """
 
 import numpy as np
-from nbayes1b import NaiveBayes1
+from nbayes1b import NaiveBayes1, NaiveBayes2
 
 
 # load date
@@ -19,6 +19,8 @@ data = np.genfromtxt('vote_filled.tsv', dtype=np.int)
 X = data[:, :-1]
 y = data[:, -1]
 
+# NaiveBayes1
+
 # learn model
 clr = NaiveBayes1()
 clr.fit(X, y)
@@ -27,5 +29,22 @@ clr.fit(X, y)
 predict_y = clr.predict(X[:10, :])
 
 # print results
+print "NaiveBayes1"
 for i in xrange(10):
   print i, y[i], predict_y[i]
+
+
+# NaiveBayes2
+
+# learn model
+clr = NaiveBayes2()
+clr.fit(X, y)
+
+# test model
+predict_y = clr.predict(X[:10, :])
+
+# print results
+print "NaiveBayes2"
+for i in xrange(10):
+  print i, y[i], predict_y[i]
+
